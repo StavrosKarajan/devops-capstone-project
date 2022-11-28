4b8441ef57df773004a8048b01973d86fa5132c1
StavrosKarajan <stkaragi@gmail.com>
2022-11-28 09:50:14 -0500

    Added read account microservice-tests and code

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,21 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should READ a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+
+    # Test 2: test account not found
+    def test_account_not_found(self):
+        """It should test READING a single Account that is NOT found"""
+        account_id = 0
+        resp = self.client.get(
+            f"{BASE_URL}/{account_id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)        
