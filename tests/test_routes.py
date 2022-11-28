f8d5948d0fac8e9b069e41004742e9c923476c27
StavrosKarajan <stkaragi@gmail.com>
2022-11-28 10:57:21 -0500

    commit testing error handlers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -173,4 +173,10 @@ class TestAccountService(TestCase):
         resp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(len(data), 5)        
+        self.assertEqual(len(data), 5)   
+
+    # Test 6: Error handlers
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)             
